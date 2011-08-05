a3d853bdbec760e102f8620ad944f9b51833f928
Mattias Nordberg <mattias.nordberg@gmail.com>
2011-08-05 15:03:39 +0100

    Updated msbuild script so that it's compatible with xbuild.

--- src/AssemblyInfo.cs
+++ src/AssemblyInfo.cs
@@ -8,9 +8,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("NSimpleBus")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("NSimpleBus")]
-[assembly: AssemblyCopyright("Copyright © Microsoft 2011")]
+[assembly: AssemblyCopyright("")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 

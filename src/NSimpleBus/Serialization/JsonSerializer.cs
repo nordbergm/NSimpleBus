9070a8d627fdbb813121a43f876fc2fd132f902e
Mattias Nordberg <mattias.nordberg@gmail.com>
2011-09-19 14:25:38 +0100

    - Replaced ServiceStack.Text JSON serializer with Newtonsoft Json.Net.
    - Replaced openwrap with NuGet.

--- src/NSimpleBus/Serialization/JsonSerializer.cs
+++ src/NSimpleBus/Serialization/JsonSerializer.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using Newtonsoft.Json;
 
 namespace NSimpleBus.Serialization
 {
@@ -7,17 +8,24 @@ namespace NSimpleBus.Serialization
     {
         public Stream Serialize(object o)
         {
-            Stream s = new MemoryStream();
-            ServiceStack.Text.JsonSerializer.SerializeToStream(o, s);
+            var ms = new MemoryStream();
+            var writer = new JsonTextWriter(new StreamWriter(ms));
+            var serializer = Newtonsoft.Json.JsonSerializer.Create(new JsonSerializerSettings());
 
-            s.Seek(0, SeekOrigin.Begin);
+            serializer.Serialize(writer, o);
+            writer.Flush();
 
-            return s;
+            ms.Seek(0, SeekOrigin.Begin);
+
+            return ms;
         }
 
         public object Deserialize(Stream s, Type asType)
         {
-            return ServiceStack.Text.JsonSerializer.DeserializeFromStream(asType, s);
+            var reader = new JsonTextReader(new StreamReader(s));
+            var serializer = Newtonsoft.Json.JsonSerializer.Create(new JsonSerializerSettings());
+            
+            return serializer.Deserialize(reader, asType);
         }
     }
 }

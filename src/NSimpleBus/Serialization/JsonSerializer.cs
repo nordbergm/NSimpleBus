349a000e75fb512521c3c9e064a3ac6d40e580e8
Mattias Nordberg <mattias.nordberg@gmail.com>
2011-10-13 17:00:47 +0100

    Updated serialization settings to support non declared derived types.

--- src/NSimpleBus/Serialization/JsonSerializer.cs
+++ src/NSimpleBus/Serialization/JsonSerializer.cs
@@ -6,11 +6,22 @@ namespace NSimpleBus.Serialization
 {
     public class JsonSerializer : ISerializer
     {
+        private static readonly JsonSerializerSettings Settings;
+
+        static JsonSerializer()
+        {
+            Settings = new JsonSerializerSettings
+                       {
+                           NullValueHandling = NullValueHandling.Ignore,
+                           TypeNameHandling = TypeNameHandling.Auto
+                       };
+        }
+
         public Stream Serialize(object o)
         {
             var ms = new MemoryStream();
             var writer = new JsonTextWriter(new StreamWriter(ms));
-            var serializer = Newtonsoft.Json.JsonSerializer.Create(new JsonSerializerSettings());
+            var serializer = Newtonsoft.Json.JsonSerializer.Create(Settings);
 
             serializer.Serialize(writer, o);
             writer.Flush();
@@ -23,7 +34,7 @@ namespace NSimpleBus.Serialization
         public object Deserialize(Stream s, Type asType)
         {
             var reader = new JsonTextReader(new StreamReader(s));
-            var serializer = Newtonsoft.Json.JsonSerializer.Create(new JsonSerializerSettings());
+            var serializer = Newtonsoft.Json.JsonSerializer.Create(Settings);
             
             return serializer.Deserialize(reader, asType);
         }
